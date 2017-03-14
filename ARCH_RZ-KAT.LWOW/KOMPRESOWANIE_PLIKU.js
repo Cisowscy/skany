5838b84b50af173cab89c532216ccaf4c54bc75e
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2017-03-14 22:07:09 +0100

    Update KOMPRESOWANIE_PLIKU.js

--- ARCH_RZ-KAT.LWOW/KOMPRESOWANIE_PLIKU.js
+++ ARCH_RZ-KAT.LWOW/KOMPRESOWANIE_PLIKU.js
@@ -1,6 +1,7 @@
 var NAZW_PLIKU ="GEO";
+var TYP_PLIKU = ".json";
 
 var fs = require("fs");
-var BAZA = JSON.parse(fs.readFileSync(NAZW_PLIKU+".json", "utf8"));
-var NOWY = fs.createWriteStream(NAZW_PLIKU+"_slim.json", {flags: "w"});
+var BAZA = JSON.parse(fs.readFileSync(NAZW_PLIKU+TYP_PLIKU, "utf8"));
+var NOWY = fs.createWriteStream(NAZW_PLIKU+"_slim"+TYP_PLIKU, {flags: "w"});
 NOWY.write(JSON.stringify(BAZA));
