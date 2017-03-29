e45c2881042e241f54db756fdfa54ba54944068b
Jan Roman Jerzy Szczepan Cisowski Sas <janromancisowski@cisowscy.com>
2017-03-29 15:48:12 -0700

    korekta cd.1

---
+++ ClientApp/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    [MaterialModule]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
