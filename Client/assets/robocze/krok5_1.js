769409d8dcc39ae5cb850828bf6ad937b4b27b0a
Jan Roman J.S. Cisowski Sas <janromancisowski@cisowscy.com>
2017-04-05 22:40:39 +0200

    usuwanie2

--- Client/assets/robocze/krok5_1.js
+++
@@ -1,2 +0,0 @@
-var x = 4.3455;
-console.log(parseInt(parseFloat(x).toString().split('.')[1], 10)); 
--- Client/assets/robocze/krok5_W.js
+++
@@ -1,4 +0,0 @@
-require("babel-core").transform("code", {
-    presets: ["es2017"]
-});
-import $ from "jquery";
