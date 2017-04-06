1b1c8c9f69b56e68b92f10145c9bb5c3e504dde0
Jan Roman Jerzy Szczepan Cisowski Sas <janromancisowski@cisowscy.com>
2017-04-06 14:39:29 +0200

    VS2017 (nadal errore :/ )

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.IO; //dodane pozniej
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Antiforgery; //dodane pozniej
+using Microsoft.Extensions.Configuration; //dodane pozniej
+using Microsoft.AspNetCore.NodeSevices; //dodane pozniej
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -16,14 +22,17 @@ namespace skany
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+           services.AddMvc(); //dodane pozniej
+          services.AddNodeServices(); //dodane pozniej
+         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+            loggerFactory.AddDebug(); //dodane pozniej
 
-            if (env.IsDevelopment())
+          if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
